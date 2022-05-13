48fea280f1c489ee0360f04b7393d03bd6c788b6
Yaroslav Kukhar <kukhar.yr@gmail.com>
2022-05-13 14:57:13 +0300

    Inplemented user data storage

---
+++ iOSApp/iOSApp/Source/Core/UserData.swift
@@ -0,0 +1,35 @@
+//
+//  UserData.swift
+//  iOSApp
+//
+//  Created by Yaroslav Kukhar on 13.05.2022.
+//
+
+import Foundation
+import Firebase
+
+class UserData {
+    public static var shared = UserData()
+    
+    private let ref = Database.database().reference()
+    
+    var userUid: String? = Auth.auth().currentUser?.uid
+    var userName: String = "Unknown"
+    
+    
+    private init() {
+    }
+    
+    public func getUserName(completion: @escaping ((String) -> Void)) {
+        guard let uid = userUid else { return }
+        ref.child("users/\(uid)/name").getData(completion:  { error, snapshot in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let value = snapshot.value as? String else { return }
+            completion(value)
+        })
+    }
+}
+
