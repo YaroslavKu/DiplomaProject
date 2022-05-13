3e1c9ccc7e81084d4e17e7d44fbfb722b8b32b92
Yaroslav Kukhar <kukhar.yr@gmail.com>
2022-05-13 14:56:55 +0300

    Added UIColor extension

---
+++ iOSApp/iOSApp/Source/Core/Extensions/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  iOSApp
+//
+//  Created by Yaroslav Kukhar on 13.05.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let ocean = UIColor(red:0/255, green:64/255, blue:128/255, alpha:1.0)
+}
