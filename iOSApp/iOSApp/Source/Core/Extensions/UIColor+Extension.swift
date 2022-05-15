6479652bceb037d073fc0cd6867405179a81b283
Yaroslav Kukhar <kukhar.yr@gmail.com>
2022-05-16 00:49:29 +0300

    Update UIColor+Extension.swift

--- iOSApp/iOSApp/Source/Core/Extensions/UIColor+Extension.swift
+++ iOSApp/iOSApp/Source/Core/Extensions/UIColor+Extension.swift
@@ -9,4 +9,46 @@ import UIKit
 
 extension UIColor {
     static let ocean = UIColor(red:0/255, green:64/255, blue:128/255, alpha:1.0)
+    
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            self.init(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    func hexStringFromColor() -> String {
+        let components = self.cgColor.components
+        let r: CGFloat = components?[0] ?? 0.0
+        let g: CGFloat = components?[1] ?? 0.0
+        let b: CGFloat = components?[2] ?? 0.0
+
+        let hexString = String.init(format: "#%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
+        return hexString
+    }
+    
+    static func pickColor(alphabet: Character) -> UIColor {
+        let alphabetColors = [0x5A8770, 0xB2B7BB, 0x6FA9AB, 0xF5AF29, 0x0088B9, 0xF18636, 0xD93A37, 0xA6B12E, 0x5C9BBC, 0xF5888D, 0x9A89B5, 0x407887, 0x9A89B5, 0x5A8770, 0xD33F33, 0xA2B01F, 0xF0B126, 0x0087BF, 0xF18636, 0x0087BF, 0xB2B7BB, 0x72ACAE, 0x9C8AB4, 0x5A8770, 0xEEB424, 0x407887]
+        let str = String(alphabet).unicodeScalars
+        let unicode = Int(str[str.startIndex].value)
+        if 65...90 ~= unicode {
+            let hex = alphabetColors[unicode - 65]
+            return UIColor(red: CGFloat(Double((hex >> 16) & 0xFF)) / 255.0, green: CGFloat(Double((hex >> 8) & 0xFF)) / 255.0, blue: CGFloat(Double((hex >> 0) & 0xFF)) / 255.0, alpha: 1.0)
+        }
+        return .black
+    }
 }
