61d85dc3850092e1d5d4ff93817ed5a45161bf68
Yaroslav Kukhar <kukhar.yr@gmail.com>
2022-05-16 00:49:13 +0300

    Fixed crach

--- iOSApp/iOSApp/SceneDelegate.swift
+++ iOSApp/iOSApp/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        FirebaseApp.configure()
+//        FirebaseApp.configure()
         Auth.auth().addStateDidChangeListener { (auth, user) in
             if user == nil {
                 self.showModalAuth()
