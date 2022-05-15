310f23ffdf531f56c6b415bcba4d68571550db28
Yaroslav Kukhar <kukhar.yr@gmail.com>
2022-05-16 00:50:35 +0300

    Create TextTableViewCell.swift

---
+++ iOSApp/iOSApp/Source/Components/TextTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextTableViewCell.swift
+//  iOSApp
+//
+//  Created by Yaroslav Kukhar on 16.05.2022.
+//
+
+import UIKit
+
+class TextTableViewCell: UITableViewCell {
+    static let identifier = "TextTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
