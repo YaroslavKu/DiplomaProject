cc6935bb9fb342d9b96ea732261e7f633b4ab31b
Yaroslav Kukhar <kukhar.yr@gmail.com>
2022-05-17 01:44:53 +0300

    Update TextTableViewCell.swift

--- iOSApp/iOSApp/Source/Components/TextTableViewCell.swift
+++ iOSApp/iOSApp/Source/Components/TextTableViewCell.swift
@@ -9,16 +9,45 @@ import UIKit
 
 class TextTableViewCell: UITableViewCell {
     static let identifier = "TextTableViewCell"
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    private var textLabelView: UILabel = UILabel()
+    
+    private var text: String? {
+        didSet {
+            textLabelView.text = text
+        }
     }
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        textLabelView.clipsToBounds = true
+        contentView.addSubview(textLabelView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabelView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        textLabelView.text = nil
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
+    public func configure(with text: String) {
+        self.text = text
+    }
 }
